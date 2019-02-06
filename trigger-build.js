171398f687fb4f7d9b21588a1ca530f3456de68b
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-02-06 12:44:12 +0100

    [WIP] Support trigger build win64 repo

---
+++ trigger-build.js
@@ -0,0 +1,33 @@
+var Travis = require('travis-ci');
+
+// change this
+var repo = "yildiz-online/module-window-swt-win64";
+
+var travis = new Travis({
+    version: '2.0.0'
+});
+
+travis.authenticate({
+
+    github_token: process.env.GH_TOKEN
+
+}, function (err, res) {
+    if (err) {
+        return console.error(err);
+    }
+
+    travis.repos(repo.split('/')[0], repo.split('/')[1]).builds.get(function (err, res) {
+        if (err) {
+            return console.error(err);
+        }
+
+        travis.requests.post({
+            build_id: res.builds[0].id
+        }, function (err, res) {
+            if (err) {
+                return console.error(err);
+            }
+            console.log(res.flash[0].notice);
+        });
+    });
+});
