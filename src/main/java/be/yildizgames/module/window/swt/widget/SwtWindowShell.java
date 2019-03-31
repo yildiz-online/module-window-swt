f3a7ea05ca7d9aa356c92ac27943833b924c4e21
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-03-31 17:11:43 +0200

    [WIP] Improve API.

--- src/main/java/be/yildizgames/module/window/swt/widget/SwtWindowShell.java
+++ src/main/java/be/yildizgames/module/window/swt/widget/SwtWindowShell.java
@@ -61,6 +61,7 @@ import org.eclipse.swt.widgets.Text;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Consumer;
 
 public class SwtWindowShell implements WindowShell {
 
